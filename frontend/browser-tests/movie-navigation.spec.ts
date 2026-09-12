bbba8635a2df98388049abeb0c43b1a6416a60f1
huhengbo <huhbo1992@126.com>
2026-09-12 19:34:32 +0800

    test(ui): fix movie navigation lint

--- frontend/browser-tests/movie-navigation.spec.ts
+++ frontend/browser-tests/movie-navigation.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, type Page, type Route } from '@playwright/test';
+import { expect, test, type Route } from '@playwright/test';
 import { URL } from 'node:url';
 
 const task = {
